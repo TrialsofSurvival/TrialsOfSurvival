925ca30901f174c530a74669f1be8cc11de81436
Joshua <35875125+TrialsofSurvival@users.noreply.github.com>
2018-01-27 16:59:34 -0500

    Added Attack

--- Trials Of Survival/Assets/Player.cs
+++ Trials Of Survival/Assets/Player.cs
@@ -12,22 +12,33 @@ public class Player : MonoBehaviour {
 
 	public Rigidbody2D ninja;
 
+	public Camera cam;
+
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
-	
+		anim = this.GetComponent <Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		if (cam.transform.position.y <= -81 || cam.transform.position.y >= 82) {
+
+		} else {
+			cam.transform.position = new Vector3 (0, this.gameObject.transform.position.y, -1250);
+		}
 		//grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 		
 		//do this
 		if (Input.GetKey (Left)) {
-			gameObject.transform.localScale = new Vector3(Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+			gameObject.transform.localScale = new Vector3(-Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);
 			ninja.velocity = new Vector2 (-speed, ninja.velocity.y);
+			anim.Play ("Walk");
 		} else if (Input.GetKey (Right)) {
-			gameObject.transform.localScale = new Vector3(-Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);;
+			gameObject.transform.localScale = new Vector3(Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);;
 			ninja.velocity = new Vector2 (+speed, ninja.velocity.y); 
+			anim.Play ("Walk");
 		} else if (Input.GetKey (Up) && upAndDown) {
 			ninja.velocity = new Vector2 (ninja.velocity.x, +speed);
 		} else if (Input.GetKey (Down) && upAndDown) {
@@ -37,6 +48,16 @@ public class Player : MonoBehaviour {
 		else
 		{
 			ninja.velocity = new Vector2(0,ninja.velocity.y);
+		}
+
+		if (Input.GetKeyUp (Left)) {
+			anim.Play ("Nothing");
+		} else if (Input.GetKeyUp (Right)) {
+			anim.Play ("Nothing");
+		}
+
+		if (Input.GetButtonDown ("Fire1")) {
+			anim.Play ("Sword");
 		}
 	}
 }
